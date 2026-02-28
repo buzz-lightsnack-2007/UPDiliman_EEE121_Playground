937435222f91d4997d2c463c8c01d3078c125ebf
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2026-03-01 07:48:22 +0800

    Feat: Modify week 6 for 20%

--- src/week6/bingen.cpp
+++ src/week6/bingen.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 vector <vector <int>> allb;
 
+/**
+ * Generates 3-ary numbers of length n. 
+ * 
+ * @param bin (vector<int>) binary
+ * @param n (int) size
+ */
 void bingen(vector <int> bin ,int n){
 	if(bin.size() == n){
 		allb.push_back(bin);
 	}else{
-		for(int i = 0; i < 2; i++){
+		// Each entry can have any integer from 0 to 3; loop it through
+		for(int i = 0; i < 3; i++){ // Take this! 
 			bin.push_back(i);
 			bingen(bin, n);
 			bin.pop_back();
